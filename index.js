c796f64234204ae71f10c70b1228bade1d3b30b9
Eva Fridman <fridman.1999@mail.ru>
2026-07-13 16:38:39 +0300

    release 2

--- index.js
+++ index.js
@@ -13,6 +13,38 @@ const studyTimer = new Stopwatch(labels[2]);
 
 workTimer.seconds = 10;
 
-console.log(workTimer.seconds);
-console.log(restTimer.seconds);
-console.log(studyTimer.seconds);
+console.log(workTimer.seconds); // 10
+console.log(restTimer.seconds); // 0
+console.log(studyTimer.seconds); // 0
+
+Stopwatch.prototype.tick = function() {
+    this.seconds++
+    return this;
+};
+
+Stopwatch.prototype.reset = function() {
+    this.seconds = 0;
+    return this;
+};
+
+Stopwatch.prototype.format = function(seconds = this.seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${this.label}: ${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+};
+
+console.log(workTimer.tick()); // Stopwatch {label: 'Работа', seconds: 11}
+console.log(workTimer.format()); // Работа: 00:11
+console.log(workTimer.reset()); // Stopwatch {label: 'Работа', seconds: 0}
+
+console.log(restTimer.tick()); // Stopwatch {label: 'Отдых', seconds: 1}
+console.log(restTimer.format()); // Отдых: 00:01
+console.log(restTimer.reset()); // Stopwatch {label: 'Отдых', seconds: 0}
+
+console.log(studyTimer.tick()); // Stopwatch {label: 'Учёба', seconds: 1}
+console.log(studyTimer.format()); // Учёба: 00:01
+console.log(studyTimer.reset()); // Stopwatch {label: 'Учёба', seconds: 0}
+
+console.log(workTimer.tick().tick().tick().format()); // Работа: 00:03
+console.log(restTimer.tick().tick().tick().tick().format()); // Отдых: 00:04
+console.log(studyTimer.tick().tick().tick().tick().tick().format()); // Учёба: 00:05
